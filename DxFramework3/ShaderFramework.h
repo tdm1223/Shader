e749358ae01f434a21ea8bc183f9c90f249435af
SGerm <tdm1223@naver.com>
2018-08-03 23:42:07 +0900

    20180803
    외곽선 찾기와 양각 효과

---
+++ DxFramework3/ShaderFramework.h
@@ -0,0 +1,44 @@
+//**********************************************************************
+//
+// ShaderFramework.h
+// 
+//
+//**********************************************************************
+
+
+#pragma once
+
+#include <d3d9.h>
+#include <d3dx9.h>
+
+// ---------- 선언 ------------------------------------
+#define WIN_WIDTH		800
+#define WIN_HEIGHT		600
+
+// ---------------- 함수 프로토타입 ------------------------
+
+// 메시지 처리기 관련
+LRESULT WINAPI MsgProc( HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam );
+void ProcessInput(HWND hWnd, WPARAM keyPress);
+
+// 초기화 과련
+bool InitEverything(HWND hWnd);
+bool InitD3D(HWND hWnd);
+bool LoadAssets();
+LPD3DXEFFECT LoadShader( const char * filename );
+LPDIRECT3DTEXTURE9 LoadTexture(const char * filename);
+LPD3DXMESH LoadModel(const char * filename);
+
+// 게임루프 관련
+void PlayDemo();
+void Update();
+
+// 렌더링 관련
+void RenderFrame();
+void RenderScene();
+void RenderInfo();
+
+// 뒷정리 관련
+void Cleanup();
+
+void InitFullScreenQuad();
